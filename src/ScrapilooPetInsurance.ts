a63770ef7561e2bb6d3be8be4b1f71a96c9df35a
Carl Viktor Svensson <carl.viktor.svensson@gmail.com>
2023-10-23 13:36:15 +0200

    pet insurance

---
+++ src/ScrapilooPetInsurance.ts
@@ -0,0 +1,129 @@
+/**
+ * @author     Carl Viktor Svensson
+ * @author     Kelsie Maria Enqvist
+ * @license    Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+ */
+
+import {
+  BooleanType,
+  CurrencyType,
+  IntegerType,
+  StringType,
+  TimeType,
+  URLType,
+  UnitType,
+} from ".";
+
+export interface ScrapilooPetInsurance {
+  readonly __key?: string;
+  readonly compensation?: {
+    readonly currency?: CurrencyType;
+    readonly max?: IntegerType;
+    readonly min?: IntegerType;
+  };
+  readonly deductible_period?: {
+    readonly time?: IntegerType;
+    readonly unit?: UnitType;
+  };
+  readonly deductible?: {
+    readonly fixed?: {
+      readonly currency?: CurrencyType;
+      readonly max?: IntegerType;
+      readonly min?: IntegerType;
+    };
+    readonly variable?: {
+      readonly max?: IntegerType;
+      readonly min?: IntegerType;
+      readonly unit?: UnitType;
+    };
+  };
+  readonly direct_settlement?: BooleanType;
+  /**
+   * Primary email address
+   */
+  readonly email?: StringType;
+  /**
+   * online vet appointments included
+   */
+  readonly free_vet_consultation?: BooleanType;
+  readonly grace_period?: {
+    readonly time?: IntegerType;
+    readonly unit?: UnitType;
+  };
+  readonly hidden_fault_insurance?: BooleanType;
+  readonly life_insurance?: {
+    readonly available?: BooleanType;
+    readonly compensation?: {
+      readonly max?: IntegerType;
+      readonly min?: IntegerType;
+    };
+  };
+  readonly location?: {
+    /**
+     * Street address
+     */
+    readonly address?: StringType;
+    /**
+     * City
+     */
+    readonly city?: StringType;
+    /**
+     * Country
+     */
+    readonly country?: StringType;
+    /**
+     * ZIP code
+     */
+    readonly zip?: StringType;
+  };
+  readonly name?: StringType;
+  readonly opening_hours?: {
+    readonly friday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+    readonly monday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+    readonly saturday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+    readonly sunday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+    readonly thursday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+    readonly tuesday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+    readonly wednesday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+  };
+  /**
+   * Organization number
+   */
+  readonly organization_number?: StringType;
+  /**
+   * Phone number
+   */
+  readonly phone?: StringType;
+  readonly tracking?: {
+    readonly active?: BooleanType;
+    readonly main?: URLType;
+  };
+}
---
+++ src/ScrapilooPetInsuranceImpl.ts
@@ -0,0 +1,116 @@
+import {
+  BooleanType,
+  CurrencyType,
+  IntegerType,
+  StringType,
+  TimeType,
+  URLType,
+  UnitType,
+} from ".";
+
+export class ScrapilooPetInsuranceImpl {
+  readonly __key?: string;
+  readonly compensation?: {
+    readonly currency?: CurrencyType;
+    readonly max?: IntegerType;
+    readonly min?: IntegerType;
+  };
+  readonly deductible_period?: {
+    readonly time?: IntegerType;
+    readonly unit?: UnitType;
+  };
+  readonly deductible?: {
+    readonly fixed?: {
+      readonly currency?: CurrencyType;
+      readonly max?: IntegerType;
+      readonly min?: IntegerType;
+    };
+    readonly variable?: {
+      readonly max?: IntegerType;
+      readonly min?: IntegerType;
+      readonly unit?: UnitType;
+    };
+  };
+  readonly direct_settlement?: BooleanType;
+  /**
+   * Primary email address
+   */
+  readonly email?: StringType;
+  /**
+   * online vet appointments included
+   */
+  readonly free_vet_consultation?: BooleanType;
+  readonly grace_period?: {
+    readonly time?: IntegerType;
+    readonly unit?: UnitType;
+  };
+  readonly hidden_fault_insurance?: BooleanType;
+  readonly life_insurance?: {
+    readonly available?: BooleanType;
+    readonly compensation?: {
+      readonly max?: IntegerType;
+      readonly min?: IntegerType;
+    };
+  };
+  readonly location?: {
+    /**
+     * Street address
+     */
+    readonly address?: StringType;
+    /**
+     * City
+     */
+    readonly city?: StringType;
+    /**
+     * Country
+     */
+    readonly country?: StringType;
+    /**
+     * ZIP code
+     */
+    readonly zip?: StringType;
+  };
+  readonly name?: StringType;
+  readonly opening_hours?: {
+    readonly friday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+    readonly monday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+    readonly saturday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+    readonly sunday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+    readonly thursday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+    readonly tuesday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+    readonly wednesday?: {
+      readonly from?: TimeType;
+      readonly to?: TimeType;
+    };
+  };
+  /**
+   * Organization number
+   */
+  readonly organization_number?: StringType;
+  /**
+   * Phone number
+   */
+  readonly phone?: StringType;
+  readonly tracking?: {
+    readonly active?: BooleanType;
+    readonly main?: URLType;
+  };
+}
